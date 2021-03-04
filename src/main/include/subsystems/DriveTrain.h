3c9175645c76683a043381d2a83e40c03818c9ce
GEARHEADS-6\first <firstteam102@gmail.com>
2021-03-03 19:06:33 -0500

    Reset multiplier

--- src/main/include/subsystems/DriveTrain.h
+++ src/main/include/subsystems/DriveTrain.h
@@ -61,7 +61,7 @@ class DriveTrain : public frc2::SubsystemBase {
   std::string strInput;
   int printEvery, driveMode;
   int counter = 0;
-  double spdMult = 2;
+  double spdMult = 1;
 
 
   // Components (e.g. motor controllers and sensors) should generally be
