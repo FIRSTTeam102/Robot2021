f8504a8a4306b8f737c3fc79037c76eacc18edcc
5GamAzing <ajdailey5@gmail.com>
2021-03-24 15:14:32 -0400

    Fixed startpoints for AutoNav

--- src/main/cpp/commands/auto/AutoNav.cpp
+++ src/main/cpp/commands/auto/AutoNav.cpp
@@ -13,14 +13,17 @@
 AutoNav::AutoNav(DriveTrain* pDriveTrain, int path) {
   // Add your commands here, e.g.
   // AddCommands(FooCommand(), BarCommand());
-  if (path == 1) { //Start on C2
-    AddCommands(MoveLinear(pDriveTrain, 7.5*12.0, kSpd), DragTurn(pDriveTrain, 360, 2.5*12.0, kSpd), MoveLinear(pDriveTrain, 7.5*12.0, kSpd), DragTurn(pDriveTrain, -315, 2.5*12.0, kSpd), MoveLinear(pDriveTrain, 14.1*12.0, kSpd), DragTurn(pDriveTrain, -225, 2.5*12.0, kSpd), MoveLinear(pDriveTrain, 22.5*12.0, kSpd));
+  if (path == 1) { //Start on C1
+    AddCommands(MoveLinear(pDriveTrain, 10*12, kSpd), DragTurn(pDriveTrain, 360, 2.5*12.0, kSpd), MoveLinear(pDriveTrain, 7.5*12.0, kSpd), DragTurn(pDriveTrain, -315, 2.5*12.0, kSpd), MoveLinear(pDriveTrain, 14.1*12.0, kSpd), DragTurn(pDriveTrain, -225, 2.5*12.0, kSpd), MoveLinear(pDriveTrain, 22.5*12.0, kSpd));
   }
-  else if (path == 2) { //Start on F2 @ 45 degrees
-    AddCommands(MoveLinear(pDriveTrain, 3.5*12.0, kSpd), DragTurn(pDriveTrain, 90, 10.6*12.0, kSpd), MoveLinear(pDriveTrain, 2.5*12.0, kSpd), DragTurn(pDriveTrain, -270, 2.5*12.0, kSpd), MoveLinear(pDriveTrain, 2.5*12.0, kSpd), DragTurn(pDriveTrain, 90, 10.6*12.0, kSpd), MoveLinear(pDriveTrain, 10.0*12.0, kSpd));
+  else if (path == 2) { //Start on F1
+    AddCommands(DragTurn(pDriveTrain, -45, 2.92*12.0, kSpd), DragTurn(pDriveTrain, 90, 10.6*12.0, kSpd), MoveLinear(pDriveTrain, 2.5*12.0, kSpd), DragTurn(pDriveTrain, -270, 2.5*12.0, kSpd), MoveLinear(pDriveTrain, 2.5*12.0, kSpd), DragTurn(pDriveTrain, 90, 10.6*12.0, kSpd), DragTurn(pDriveTrain, -45, 2.92*12.0, kSpd));
   }
-  else if (path == 3) { //Start on C2
-    AddCommands(DragTurn(pDriveTrain, 90, 2.5*12.0, kSpd), MoveLinear(pDriveTrain, 2.5*12.0, kSpd), MoveLinear(pDriveTrain, -2.5*12.0, -kSpd), DragTurn(pDriveTrain, 30, 2.5*12.0, -kSpd), MoveLinear(pDriveTrain, -3.6*12.0, -kSpd), DragTurn(pDriveTrain, 150, 2.5*12.0, -kSpd), MoveLinear(pDriveTrain, -7.5*12.0, -kSpd), MoveLinear(pDriveTrain, 7.5*12.0, kSpd), DragTurn(pDriveTrain, 90, 2.5*12.0, kSpd), MoveLinear(pDriveTrain, 2.5*12.0, kSpd), DragTurn(pDriveTrain, 90, 2.5*12.0, kSpd), MoveLinear(pDriveTrain, 7.5*12.0, kSpd), DragTurn(pDriveTrain, 90, 5*12, -kSpd));
+  else if (path == 3) { //Start on C1
+    AddCommands(MoveLinear(pDriveTrain, 2.5*12.0, kSpd), DragTurn(pDriveTrain, 90, 2.5*12.0, kSpd), MoveLinear(pDriveTrain, 2.5*12.0, kSpd), MoveLinear(pDriveTrain, -2.5*12.0, -kSpd), DragTurn(pDriveTrain, 30, 2.5*12.0, -kSpd), MoveLinear(pDriveTrain, -3.6*12.0, -kSpd), DragTurn(pDriveTrain, 150, 2.5*12.0, -kSpd), MoveLinear(pDriveTrain, -7.5*12.0, -kSpd), MoveLinear(pDriveTrain, 7.5*12.0, kSpd), DragTurn(pDriveTrain, 90, 2.5*12.0, kSpd), MoveLinear(pDriveTrain, 2.5*12.0, kSpd), DragTurn(pDriveTrain, 90, 2.5*12.0, kSpd), MoveLinear(pDriveTrain, 7.5*12.0, kSpd), DragTurn(pDriveTrain, 90, 5*12, -kSpd));
+  }
+  else if (path == 0) { //Test functions
+    AddCommands(MoveLinear(pDriveTrain, 2.5*12.0, kSpd), TurnDegrees(pDriveTrain, 180, kSpd));
   }
   else {
     printf("Invalid path\n");
